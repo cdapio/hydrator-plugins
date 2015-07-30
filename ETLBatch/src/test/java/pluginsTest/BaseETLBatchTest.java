b6f9d8075e995149a3986056a79903cfbe9ca4e7
ataylor4 <ashley@cask.co>
2015-07-30 12:14:50 -0700

    WIP

--- ETLBatch/src/test/java/pluginsTest/BaseETLBatchTest.java
+++ ETLBatch/src/test/java/pluginsTest/BaseETLBatchTest.java
@@ -21,25 +21,11 @@ import co.cask.cdap.proto.Id;
 import co.cask.cdap.template.etl.api.PipelineConfigurable;
 import co.cask.cdap.template.etl.api.batch.BatchSource;
 import co.cask.cdap.template.etl.batch.ETLBatchTemplate;
-import co.cask.cdap.template.etl.batch.sink.BatchCubeSink;
-import co.cask.cdap.template.etl.batch.sink.DBSink;
-import co.cask.cdap.template.etl.batch.sink.KVTableSink;
-import co.cask.cdap.template.etl.batch.sink.TableSink;
-import co.cask.cdap.template.etl.batch.sink.TimePartitionedFileSetDatasetAvroSink;
-import co.cask.cdap.template.etl.batch.source.DBSource;
-import co.cask.cdap.template.etl.batch.source.KVTableSource;
-import co.cask.cdap.template.etl.batch.source.StreamBatchSource;
-import co.cask.cdap.template.etl.batch.source.TableSource;
-import co.cask.cdap.template.etl.common.DBRecord;
 import co.cask.cdap.template.etl.common.ETLConfig;
-import co.cask.cdap.template.etl.transform.ProjectionTransform;
-import co.cask.cdap.template.etl.transform.ScriptFilterTransform;
-import co.cask.cdap.template.etl.transform.StructuredRecordToGenericRecordTransform;
 import co.cask.cdap.test.TestBase;
-import org.apache.avro.mapred.AvroKey;
-import org.apache.avro.mapreduce.AvroKeyOutputFormat;
 import org.junit.BeforeClass;
 import plugins.sink.ElasticsearchSink;
+import plugins.source.StreamBatchSource;
 
 import java.io.IOException;
 
@@ -53,12 +39,7 @@ public class BaseETLBatchTest extends TestBase {
   @BeforeClass
   public static void setupTest() throws IOException {
     addTemplatePlugins(TEMPLATE_ID, "batch-plugins-1.0.0.jar",
-                       DBSource.class, KVTableSource.class, StreamBatchSource.class, TableSource.class, DBRecord.class,
-                       BatchCubeSink.class, DBSink.class, KVTableSink.class, TableSink.class,
-                       TimePartitionedFileSetDatasetAvroSink.class, AvroKeyOutputFormat.class, AvroKey.class,
-                       ElasticsearchSink.class);
-    addTemplatePlugins(TEMPLATE_ID, "transforms-1.0.0.jar",
-                       ProjectionTransform.class, ScriptFilterTransform.class, StructuredRecordToGenericRecordTransform.class);
+                       StreamBatchSource.class, ElasticsearchSink.class);
     deployTemplate(NAMESPACE, TEMPLATE_ID, ETLBatchTemplate.class,
                    PipelineConfigurable.class.getPackage().getName(),
                    BatchSource.class.getPackage().getName(),
