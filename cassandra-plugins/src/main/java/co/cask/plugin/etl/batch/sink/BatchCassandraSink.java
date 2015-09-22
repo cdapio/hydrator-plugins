b5bf67f19fee43a9bdd2de35842ea90e6e4301c9
John Jackson <john@cask.co>
2015-09-21 20:20:38 -0700

    Update BatchCassandraSink.java
    Fixes.

--- cassandra-plugins/src/main/java/co/cask/plugin/etl/batch/sink/BatchCassandraSink.java
+++ cassandra-plugins/src/main/java/co/cask/plugin/etl/batch/sink/BatchCassandraSink.java
@@ -51,7 +51,7 @@ import javax.annotation.Nullable;
 @Name("Cassandra")
 @Description("CDAP Cassandra Batch Sink takes the structured record from the input source " +
   "and converts each field to a byte buffer, then puts it in the keyspace and column family specified by the user. " +
-  "The Cassandra server should be running prior to creating the adapter, " +
+  "The Cassandra server should be running prior to creating the application, " +
   "and the keyspace and column family should be created.")
 public class BatchCassandraSink extends BatchSink<StructuredRecord, Map<String, ByteBuffer>, List<ByteBuffer>> {
   private final CassandraBatchConfig config;
@@ -133,20 +133,20 @@ public class BatchCassandraSink extends BatchSink<StructuredRecord, Map<String, 
 
     @Name(Cassandra.PORT)
     @Nullable
-    @Description("The rpc port for Cassandra. For example, 9160. " +
+    @Description("The RPC port for Cassandra. For example, 9160. " +
       "Please also check the configuration to make sure that start_rpc is true in cassandra.yaml")
     private Integer port;
 
     @Name(Cassandra.COLUMN_FAMILY)
-    @Description("The column family to inject data into. Create the column family before starting the adapter.")
+    @Description("The column family to inject data into. Create the column family before starting the application.")
     private String columnFamily;
 
     @Name(Cassandra.KEYSPACE)
-    @Description("The keyspace to inject data into. Create the keyspace before starting the adapter.")
+    @Description("The keyspace to inject data into. Create the keyspace before starting the application.")
     private String keyspace;
 
     @Name(Cassandra.INITIAL_ADDRESS)
-    @Description("The initial address to connect to. For example, \"10.11.12.13\".")
+    @Description("The initial address to connect to. For example: \"10.11.12.13\".")
     private String initialAddress;
 
     @Name(Cassandra.COLUMNS)
@@ -155,7 +155,7 @@ public class BatchCassandraSink extends BatchSink<StructuredRecord, Map<String, 
     private String columns;
 
     @Name(Cassandra.PRIMARY_KEY)
-    @Description("A comma-separated list of primary keys. For example, \"key1,key2\"")
+    @Description("A comma-separated list of primary keys. For example: \"key1,key2\"")
     private String primaryKey;
 
     public CassandraBatchConfig(String partitioner, @Nullable Integer port, String columnFamily, String keyspace,
