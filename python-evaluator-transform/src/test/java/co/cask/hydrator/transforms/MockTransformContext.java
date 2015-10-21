5defe9c09340ffa30c4b9b4709c1eabfa6fc7f41
Ali Anwar <anwar1@berkeley.edu>
2015-10-20 22:36:31 -0700

    PythonEvaluator transform

---
+++ python-evaluator-transform/src/test/java/co/cask/hydrator/transforms/MockTransformContext.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.hydrator.transforms;
+
+import co.cask.cdap.api.metrics.Metrics;
+import co.cask.cdap.api.plugin.PluginProperties;
+import co.cask.cdap.etl.api.TransformContext;
+import co.cask.cdap.etl.common.MockMetrics;
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * Mock context for unit tests
+ * TODO: remove once CDAP-4027 is resolved.
+ */
+public class MockTransformContext implements TransformContext {
+  private final PluginProperties pluginProperties;
+  private final Metrics metrics;
+
+  public MockTransformContext(Map<String, String> args) {
+    this(args, new MockMetrics());
+  }
+
+  public MockTransformContext(Map<String, String> args, Metrics metrics) {
+    this.pluginProperties = PluginProperties.builder().addAll(args).build();
+    this.metrics = metrics;
+  }
+
+  public MockTransformContext() {
+    this(Maps.<String, String>newHashMap());
+  }
+
+  @Override
+  public PluginProperties getPluginProperties() {
+    return pluginProperties;
+  }
+
+  @Override
+  public PluginProperties getPluginProperties(String pluginId) {
+    return null;
+  }
+
+  @Override
+  public Metrics getMetrics() {
+    return metrics;
+  }
+
+  @Override
+  public int getStageId() {
+    return 1;
+  }
+
+  @Override
+  public <T> T newPluginInstance(String pluginId) throws InstantiationException {
+    return null;
+  }
+
+  @Override
+  public <T> Class<T> loadPluginClass(String pluginId) {
+    return null;
+  }
+}
