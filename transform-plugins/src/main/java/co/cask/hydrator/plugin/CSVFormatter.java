0208b60e1b65c1d2e9ae53184e1392a53f6d42ea
Nitin Motgi <nitin@continuuity.com>
2015-10-25 23:35:33 -0700

    Adding in transform plugins. CSVFormatter is added to begin with.

---
+++ transform-plugins/src/main/java/co/cask/hydrator/plugin/CSVFormatter.java
@@ -0,0 +1,7 @@
+package co.cask.hydrator.plugin;
+
+/**
+ * Created by nmotgi on 10/25/15.
+ */
+public class CSVFormatter {
+}
---
+++ transform-plugins/src/test/java/co/cask/hydrator/plugin/CSVFormatterTest.java
@@ -0,0 +1,10 @@
+package co.cask.hydrator.plugin;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nmotgi on 10/25/15.
+ */
+public class CSVFormatterTest {
+
+}
