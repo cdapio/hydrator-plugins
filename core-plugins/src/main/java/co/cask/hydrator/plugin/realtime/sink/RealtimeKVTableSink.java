68b032fb6cbedaf24ba6d1fdfc8f68cedb72a4cf
Kashif Nazir <kashif@cask.co>
2016-06-22 18:09:52 -0700

    [CDAP-3069] Wrote RealtimeKVTableSink and unit tests

--- core-plugins/src/main/java/co/cask/hydrator/plugin/realtime/sink/RealtimeKVTableSink.java
+++ core-plugins/src/main/java/co/cask/hydrator/plugin/realtime/sink/RealtimeKVTableSink.java
@@ -20,12 +20,26 @@ package co.cask.hydrator.plugin.realtime.sink;
 import co.cask.cdap.api.annotation.Description;
 import co.cask.cdap.api.annotation.Name;
 import co.cask.cdap.api.annotation.Plugin;
+import co.cask.cdap.api.common.Bytes;
 import co.cask.cdap.api.data.format.StructuredRecord;
+import co.cask.cdap.api.data.schema.Schema;
+import co.cask.cdap.api.dataset.DatasetProperties;
+import co.cask.cdap.api.dataset.lib.KeyValue;
+import co.cask.cdap.api.dataset.lib.KeyValueTable;
+import co.cask.cdap.api.dataset.table.Table;
 import co.cask.cdap.api.plugin.PluginConfig;
+import co.cask.cdap.etl.api.PipelineConfigurer;
 import co.cask.cdap.etl.api.realtime.DataWriter;
 import co.cask.cdap.etl.api.realtime.RealtimeSink;
+import co.cask.cdap.format.RecordPutTransformer;
+import co.cask.hydrator.common.SchemaValidator;
 import co.cask.hydrator.plugin.common.Properties;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.Nullable;
 
 /**
@@ -33,7 +47,7 @@ import javax.annotation.Nullable;
  */
 @Plugin(type = "realtimesink")
 @Name("KVTable")
-@Description("Real-time Sink for CDAP KV Table dataset")
+@Description("Real-time Sink for CDAP KVTable dataset")
 public class RealtimeKVTableSink extends RealtimeSink<StructuredRecord> {
 
   private static final String NAME_DESC = "Name of the dataset. If it does not already exist, one will be created.";
@@ -43,7 +57,7 @@ public class RealtimeKVTableSink extends RealtimeSink<StructuredRecord> {
   /**
    * Config class for KVTableSink
    */
-  public static class RealtimeKVTableConfig extends PluginConfig {
+  public static class RealtimeKVTableSinkConfig extends PluginConfig {
     @Description(NAME_DESC)
     private String name;
 
@@ -57,26 +71,101 @@ public class RealtimeKVTableSink extends RealtimeSink<StructuredRecord> {
     @Nullable
     private String valueField;
 
-    public RealtimeKVTableConfig() {
+    public RealtimeKVTableSinkConfig() {
       this(null, Properties.KeyValueTable.DEFAULT_KEY_FIELD, Properties.KeyValueTable.DEFAULT_VALUE_FIELD);
     }
 
-    public RealtimeKVTableConfig(String name, String keyField, String valueField) {
+    public RealtimeKVTableSinkConfig(String name, String keyField, String valueField) {
       this.name = name;
       this.keyField = keyField;
       this.valueField = valueField;
     }
   }
 
-  private final RealtimeKVTableConfig realtimeKVTableConfig;
+  private final RealtimeKVTableSinkConfig realtimeKVTableSinkConfig;
+  private RecordPutTransformer recordPutTransformer;
 
-  public RealtimeKVTableSink(RealtimeKVTableConfig realtimeKVTableConfig) {
-    this.realtimeKVTableConfig = realtimeKVTableConfig;
+  public RealtimeKVTableSink(RealtimeKVTableSinkConfig realtimeKVTableConfig) {
+    this.realtimeKVTableSinkConfig = realtimeKVTableConfig;
+  }
+
+  @Override
+  public void configurePipeline(PipelineConfigurer pipelineConfigurer) {
+    super.configurePipeline(pipelineConfigurer);
+    Schema inputSchema = pipelineConfigurer.getStageConfigurer().getInputSchema();
+    // validate that input and output fields are present
+    if (inputSchema != null) {
+      SchemaValidator.validateFieldsArePresentInSchema(inputSchema, realtimeKVTableSinkConfig.keyField);
+      SchemaValidator.validateFieldsArePresentInSchema(inputSchema, realtimeKVTableSinkConfig.valueField);
+      validateSchemaTypeIsStringOrBytes(inputSchema, realtimeKVTableSinkConfig.keyField, false);
+      validateSchemaTypeIsStringOrBytes(inputSchema, realtimeKVTableSinkConfig.valueField, true);
+    }
+  }
+
+  private void validateSchemaTypeIsStringOrBytes(Schema inputSchema, String fieldName, boolean isNullable) {
+    Schema fieldSchema = inputSchema.getField(fieldName).getSchema();
+    boolean fieldIsNullable = fieldSchema.isNullable();
+    if (!isNullable && fieldIsNullable) {
+      throw new IllegalArgumentException("Field " + fieldName + " cannot be nullable");
+    }
+    Schema.Type fieldType = fieldIsNullable ? fieldSchema.getNonNullable().getType() : fieldSchema.getType();
+
+    if (fieldType != Schema.Type.STRING && fieldType != Schema.Type.BYTES) {
+      throw new IllegalArgumentException(
+        String.format("Field name %s is of type %s, only types String and Bytes are supported for KVTable",
+                      fieldName, fieldType));
+    }
   }
 
   @Override
   public int write(Iterable<StructuredRecord> records, DataWriter writer) throws Exception {
-    return 0;
+    KeyValueTable table = writer.getDataset(realtimeKVTableSinkConfig.name);
+    int numRecords = 0;
+    for (StructuredRecord record : records) {
+      // get key bytes
+      Object key = record.get(realtimeKVTableSinkConfig.keyField);
+      Preconditions.checkArgument(key != null, "Key cannot be null.");
+      byte[] keyBytes;
+
+      Schema keyFieldSchema = record.getSchema().getField(realtimeKVTableSinkConfig.keyField).getSchema();
+      if (keyFieldSchema.getType().equals(Schema.Type.STRING)) {
+        keyBytes = Bytes.toBytes((String) key);
+      } else if (keyFieldSchema.getType().equals(Schema.Type.BYTES)) {
+          keyBytes = key instanceof ByteBuffer ? Bytes.toBytes((ByteBuffer) key) : (byte[]) key;
+      } else {
+        throw new Exception(
+          String.format("Key field %s cannot have schema %s. It must be of either String or Bytes",
+                        realtimeKVTableSinkConfig.keyField, keyFieldSchema));
+      }
+
+      // get value bytes
+      Schema.Field valueFieldSchema = record.getSchema().getField(realtimeKVTableSinkConfig.valueField);
+      if (valueFieldSchema == null) {
+        throw new Exception(
+          String.format("Value Field %s is missing in the input record", realtimeKVTableSinkConfig.valueField));
+      }
+
+      byte[] valBytes = null;
+      Object val = record.get(realtimeKVTableSinkConfig.valueField);
+      if (val != null) {
+        Schema.Type valueFieldType =
+          valueFieldSchema.getSchema().isNullable() ? valueFieldSchema.getSchema().getNonNullable().getType() :
+            valueFieldSchema.getSchema().getType();
+        if (valueFieldType.equals(Schema.Type.STRING)) {
+          valBytes = Bytes.toBytes((String) val);
+        } else if (valueFieldType.equals(Schema.Type.BYTES)) {
+          valBytes = val instanceof ByteBuffer ? Bytes.toBytes((ByteBuffer) val) : (byte[]) val;
+        } else {
+          throw new Exception(
+            String.format("Value field %s cannot have schema %s. It must be of either String or Bytes",
+                          realtimeKVTableSinkConfig.valueField, valueFieldSchema));
+        }
+      }
+
+      table.write(new KeyValue<byte[], byte[]>(keyBytes, valBytes));
+      numRecords++;
+    }
+    return numRecords;
   }
 
 }
---
+++ core-plugins/src/test/java/co/cask/hydrator/plugin/realtime/sink/RealtimeKVTableSinkTest.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.hydrator.plugin.realtime.sink;
+
+import co.cask.cdap.api.data.schema.Schema;
+import co.cask.hydrator.plugin.transform.MockPipelineConfigurer;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests for real time key value table sink, with valid and invalid (input, output) schemas
+ */
+public class RealtimeKVTableSinkTest {
+  @Test(expected = IllegalArgumentException.class)
+  public void testRealtimeKVTableSinkWithMissingKeyField() {
+    Schema inputSchema = Schema.recordOf(
+      "purchase",
+      Schema.Field.of("uniqueId", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("user", Schema.of(Schema.Type.STRING))
+    );
+
+    RealtimeKVTableSink.RealtimeKVTableSinkConfig realtimeKVTableConfig =
+      new RealtimeKVTableSink.RealtimeKVTableSinkConfig("purchases", "rowKey", "user");
+    RealtimeKVTableSink realtimeKVTableSink = new RealtimeKVTableSink(realtimeKVTableConfig);
+
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(inputSchema);
+    realtimeKVTableSink.configurePipeline(mockPipelineConfigurer);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testRealtimeKVTableSinkWithMissingValueField() {
+    Schema inputSchema = Schema.recordOf(
+      "purchase",
+      Schema.Field.of("rowKey", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("userid", Schema.of(Schema.Type.STRING))
+    );
+
+    RealtimeKVTableSink.RealtimeKVTableSinkConfig realtimeKVTableConfig =
+      new RealtimeKVTableSink.RealtimeKVTableSinkConfig("purchases", "rowKey", "user");
+    RealtimeKVTableSink realtimeKVTableSink = new RealtimeKVTableSink(realtimeKVTableConfig);
+
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(inputSchema);
+    realtimeKVTableSink.configurePipeline(mockPipelineConfigurer);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testRealtimeKVTableSinkWithInvalidFieldType() {
+    Schema inputSchema = Schema.recordOf(
+      "purchase",
+      // only string and bytes are supported, this is invalid type
+      Schema.Field.of("rowKey", Schema.of(Schema.Type.LONG)),
+      Schema.Field.of("user", Schema.of(Schema.Type.STRING))
+    );
+
+    RealtimeKVTableSink.RealtimeKVTableSinkConfig realtimeKVTableConfig =
+      new RealtimeKVTableSink.RealtimeKVTableSinkConfig("purchases", "rowKey", "user");
+    RealtimeKVTableSink realtimeKVTableSink = new RealtimeKVTableSink(realtimeKVTableConfig);
+
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(inputSchema);
+    realtimeKVTableSink.configurePipeline(mockPipelineConfigurer);
+  }
+
+  @Test
+  public void testRealtimeKVTableConfigure() {
+    Schema inputSchema = Schema.recordOf(
+      "purchase",
+      Schema.Field.of("rowKey", Schema.of(Schema.Type.BYTES)),
+      Schema.Field.of("user", Schema.of(Schema.Type.STRING))
+    );
+
+    RealtimeKVTableSink.RealtimeKVTableSinkConfig realtimeKVTableConfig
+      = new RealtimeKVTableSink.RealtimeKVTableSinkConfig("purchases", "rowKey", "user");
+    RealtimeKVTableSink realtimeKVTableSink = new RealtimeKVTableSink(realtimeKVTableConfig);
+
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(inputSchema);
+    realtimeKVTableSink.configurePipeline(mockPipelineConfigurer);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testRealtimeKVTableSinkWithNullTypeForKey() {
+    Schema inputSchema = Schema.recordOf(
+      "purchase",
+      // only string and bytes are supported, this is invalid type
+      Schema.Field.of("rowKey", Schema.nullableOf(Schema.of(Schema.Type.STRING))),
+      Schema.Field.of("user", Schema.of(Schema.Type.STRING))
+    );
+
+    RealtimeKVTableSink.RealtimeKVTableSinkConfig realtimeKVTableConfig
+      = new RealtimeKVTableSink.RealtimeKVTableSinkConfig("purchases", "rowKey", "user");
+    RealtimeKVTableSink realtimeKVTableSink = new RealtimeKVTableSink(realtimeKVTableConfig);
+
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(inputSchema);
+    realtimeKVTableSink.configurePipeline(mockPipelineConfigurer);
+  }
+
+  @Test
+  public void testRealtimeKVTableSinkWithNullableTypeForValue() {
+    Schema inputSchema = Schema.recordOf(
+      "purchase",
+      // only string and bytes are supported, this is invalid type
+      Schema.Field.of("user", Schema.nullableOf(Schema.of(Schema.Type.STRING))),
+      Schema.Field.of("rowKey", Schema.of(Schema.Type.STRING))
+    );
+
+    RealtimeKVTableSink.RealtimeKVTableSinkConfig realtimeKVTableConfig
+      = new RealtimeKVTableSink.RealtimeKVTableSinkConfig("purchases", "rowKey", "user");
+    RealtimeKVTableSink realtimeKVTableSink = new RealtimeKVTableSink(realtimeKVTableConfig);
+
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(inputSchema);
+    realtimeKVTableSink.configurePipeline(mockPipelineConfigurer);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testRealtimeKVTableSinkWithNullableLongTypeForValue() {
+    Schema inputSchema = Schema.recordOf(
+      "purchase",
+      // only string and bytes are supported, this is invalid type
+      Schema.Field.of("user", Schema.nullableOf(Schema.of(Schema.Type.STRING))),
+      Schema.Field.of("rowKey", Schema.of(Schema.Type.LONG))
+    );
+
+    RealtimeKVTableSink.RealtimeKVTableSinkConfig realtimeKVTableConfig
+      = new RealtimeKVTableSink.RealtimeKVTableSinkConfig("purchases", "rowKey", "user");
+    RealtimeKVTableSink realtimeKVTableSink = new RealtimeKVTableSink(realtimeKVTableConfig);
+
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(inputSchema);
+    realtimeKVTableSink.configurePipeline(mockPipelineConfigurer);
+  }
+}
