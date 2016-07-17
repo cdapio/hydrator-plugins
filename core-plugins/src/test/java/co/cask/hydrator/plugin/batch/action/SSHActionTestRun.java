7794d81ecb51cb924d6994bf68524dda6408dd0e
Christopher Lu <gogreen.chris@gmail.com>
2016-07-16 21:44:00 -0700

    completed SSHAction Unit Tests

--- core-plugins/src/test/java/co/cask/hydrator/plugin/batch/action/SSHActionTestRun.java
+++ core-plugins/src/test/java/co/cask/hydrator/plugin/batch/action/SSHActionTestRun.java
@@ -39,6 +39,7 @@ import co.cask.cdap.test.WorkflowManager;
 import co.cask.hydrator.plugin.batch.ETLBatchTestBase;
 import com.dumbster.smtp.SimpleSmtpServer;
 import com.google.common.collect.ImmutableMap;
+import com.jcraft.jsch.JSch;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -47,6 +48,7 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
@@ -58,6 +60,7 @@ import java.security.Signature;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import javax.swing.*;
 
 
 /**
@@ -67,49 +70,32 @@ public class SSHActionTestRun extends ETLBatchTestBase {
 
   private static String host = "localhost";
   private static int port = 22;
-  private static String user = "Christopher";
-  private static String privateKeyFile = "/Users/Christopher/.ssh/id_rsa";
-  private static String privateKeyPassphrase = "thegreenfrogatcask";
-  private static String filePath = "/dirFromSSHAction/subdir/createFile.txt";
+  private static String user;
+  private static String privateKeyFile;
+  private static String privateKeyPassphrase = "pass";
+  private static String filePath;
   private static String cmd = "mkdir -p dirFromSSHAction/subdir && touch dirFromSSHAction/createFile.txt " +
     "&& mv dirFromSSHAction/createFile.txt dirFromSSHAction/subdir";
 
   @Before
-  public void beforeTest() {
-//    try {
-//      java.net.InetAddress localMachine = java.net.InetAddress.getLocalHost();
-//      host = localMachine.getHostName();
-//      KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA", "SUN");
-//      SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
-//      keyGen.initialize(2048, random);
-//      KeyPair pair = keyGen.generateKeyPair();
-//      PrivateKey priv = pair.getPrivate();
-//      PublicKey pub = pair.getPublic();
-//
-//      Signature rsa = Signature.getInstance("SHA1withRSA", "SUN");
-//      rsa.initSign(priv);
-//      FileInputStream fis = new FileInputStream("argv[0]");//what is going on here
-//      BufferedInputStream bufin = new BufferedInputStream(fis);
-//      byte[] buffer = new byte[1024];
-//      int len;
-//      while ((len = bufin.read(buffer)) >= 0) {
-//        rsa.update(buffer, 0, len);
-//      }
-//      bufin.close();
-//      byte[] realSig = rsa.sign();
-//
-//      //save signature bytes in private file
-//      FileOutputStream sigfos = new FileOutputStream("id_rsa");
-//      sigfos.write(realSig);
-//      sigfos.close();
-//      //save public key
-//      byte[] key = pub.getEncoded();
-//      FileOutputStream keyfos = new FileOutputStream("suepk");
-//      keyfos.write(key);
-//      keyfos.close();
-//    } catch (Exception e) {
-//      throw new Exception(String.format("Error when setting SSH authentication: %s", e.getMessage()));
-//    }
+  public void beforeTest() throws Exception {
+    try {
+      user = System.getProperty("user.name");
+
+      JSch jsch = new JSch();
+      String passphrase = "pass";
+      com.jcraft.jsch.KeyPair kpair = com.jcraft.jsch.KeyPair.genKeyPair(jsch, com.jcraft.jsch.KeyPair.RSA);
+      kpair.setPassphrase(passphrase);
+      kpair.writePrivateKey("/Users/" + user + "/.ssh/id_rsa");
+      kpair.writePublicKey("/Users/" + user + "/.ssh/authorized_keys", "this is the public key");
+      kpair.dispose();
+
+      privateKeyFile = "/Users/" + user + "/.ssh/id_rsa";
+      filePath = "/Users/" + user + "/dirFromSSHAction/subdir/createFile.txt";
+
+    } catch (Exception e) {
+      throw new Exception(String.format("Error when setting SSH authentication: %s", e.getMessage()));
+    }
 
   }
 
@@ -152,26 +138,69 @@ public class SSHActionTestRun extends ETLBatchTestBase {
         .addConnection(source.getName(), sink.getName())
         .build();
 
-//      AppRequest<ETLBatchConfig> appRequest = new AppRequest<>(DATAPIPELINE_ARTIFACT, etlConfig);
-//      Id.Application appId = Id.Application.from(Id.Namespace.DEFAULT, "sshActionTest");
-////      ProgramId programId = new ProgramId(Id.Namespace.DEFAULT.getId(), appId.getId(), ProgramType.WORKFLOW,
-////                                          SmartWorkflow.NAME);
-//      ApplicationManager appManager = deployApplication(appId, appRequest);
-//      WorkflowManager manager = appManager.getWorkflowManager(SmartWorkflow.NAME);
-//      manager.start(ImmutableMap.of("logical.start.time", "0"));
-//      manager.waitForFinish(3, TimeUnit.MINUTES);
-//
-//
-//      List<RunRecord> history = appManager.getHistory(new Id.Program(appId, ProgramType.WORKFLOW, SmartWorkflow.NAME),
-//                                                      ProgramRunStatus.FAILED);
-//      Assert.assertTrue(history.size() == 0);
-////      Map<String, WorkflowNodeStateDetail> nodesInFailedProgram =
-////        manager.getWorkflowNodeStates(history.get(0).getPid());
-//      //check that SSHAction node failed
-////      Assert.assertTrue(nodesInFailedProgram.containsKey());
-////      Assert.assertTrue(nodesInFailedProgram.get().getNodeStatus().equals(NodeStatus.FAILED));
-////      File f = new File(filePath);
-////      Assert.assertTrue(f.isFile());
+      AppRequest<ETLBatchConfig> appRequest = new AppRequest<>(DATAPIPELINE_ARTIFACT, etlConfig);
+      Id.Application appId = Id.Application.from(Id.Namespace.DEFAULT, "sshActionTest");
+      ApplicationManager appManager = deployApplication(appId, appRequest);
+      WorkflowManager manager = appManager.getWorkflowManager(SmartWorkflow.NAME);
+      manager.start(ImmutableMap.of("logical.start.time", "0"));
+      manager.waitForFinish(3, TimeUnit.MINUTES);
+
+      List<RunRecord> history = appManager.getHistory(new Id.Program(appId, ProgramType.WORKFLOW, SmartWorkflow.NAME),
+                                                      ProgramRunStatus.FAILED);
+      Assert.assertTrue(history.size() == 0); //make sure pipeline didn't fail
+
+      File f = new File(filePath);
+      Assert.assertTrue(f.exists());
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testFailedSSHActionPipeline() throws Exception {
+    try {
+      ImmutableMap.Builder builder = new ImmutableMap.Builder();
+      builder.put("host", host);
+      builder.put("port", Integer.toString(port));
+      builder.put("user", user);
+      builder.put("privateKeyFile", null);
+      builder.put("privateKeyPassPhrase", privateKeyPassphrase);
+      builder.put("cmd", cmd);
+      ETLStage action = new ETLStage(
+        "sshActionPOC",
+        new ETLPlugin("SSHAction", Action.PLUGIN_TYPE, builder.build(), null));
+
+      ETLStage source = new ETLStage("source",
+                                     new ETLPlugin("KVTable", BatchSource.PLUGIN_TYPE,
+                                                   ImmutableMap.of("name", "sshActionTestSource"), null));
+      ETLStage sink = new ETLStage("sink", new ETLPlugin("KVTable", BatchSink.PLUGIN_TYPE,
+                                                         ImmutableMap.of("name", "sshActionTestSink"), null));
+
+      ETLBatchConfig etlConfig = ETLBatchConfig.builder("* * * * *")
+        .addStage(source)
+        .addStage(sink)
+        .addStage(action)
+        .addConnection(action.getName(), source.getName())
+        .addConnection(source.getName(), sink.getName())
+        .build();
+
+      AppRequest<ETLBatchConfig> appRequest = new AppRequest<>(DATAPIPELINE_ARTIFACT, etlConfig);
+      Id.Application appId = Id.Application.from(Id.Namespace.DEFAULT, "sshActionTest");
+      ApplicationManager appManager = deployApplication(appId, appRequest);
+      WorkflowManager manager = appManager.getWorkflowManager(SmartWorkflow.NAME);
+      manager.start(ImmutableMap.of("logical.start.time", "0"));
+      manager.waitForFinish(3, TimeUnit.MINUTES);
+
+      List<RunRecord> history = appManager.getHistory(new Id.Program(appId, ProgramType.WORKFLOW, SmartWorkflow.NAME),
+                                                      ProgramRunStatus.FAILED);
+      Assert.assertTrue(history.size() == 1); //make sure pipeline didn't fail
+
+      Map<String, WorkflowNodeStateDetail> nodesInFailedProgram = manager.getWorkflowNodeStates(history.get(0).getPid());
+      Assert.assertTrue(nodesInFailedProgram.size() == 1);
+
+      //check that SSHAction node failed
+      Assert.assertTrue(nodesInFailedProgram.values().iterator().next().getNodeStatus().equals(NodeStatus.FAILED));
 
     } catch (Exception e) {
       e.printStackTrace();
