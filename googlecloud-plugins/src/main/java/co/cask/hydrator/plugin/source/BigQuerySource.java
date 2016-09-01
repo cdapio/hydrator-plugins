42e42aab3522769de62502fb4f3cc41a257c985e
Yue Gao <Yue@Casks-MacBook-3.local>
2016-09-01 16:22:53 -0700

    addressed comments

--- googlecloud-plugins/src/main/java/co/cask/hydrator/plugin/source/BigQuerySource.java
+++ googlecloud-plugins/src/main/java/co/cask/hydrator/plugin/source/BigQuerySource.java
@@ -22,6 +22,7 @@ import co.cask.cdap.api.annotation.Name;
 import co.cask.cdap.api.annotation.Plugin;
 import co.cask.cdap.api.data.batch.Input;
 import co.cask.cdap.api.data.format.StructuredRecord;
+import co.cask.cdap.api.data.format.UnexpectedFormatException;
 import co.cask.cdap.api.data.schema.Schema;
 import co.cask.cdap.api.dataset.lib.KeyValue;
 import co.cask.cdap.api.plugin.PluginConfig;
@@ -30,6 +31,7 @@ import co.cask.cdap.etl.api.PipelineConfigurer;
 import co.cask.cdap.etl.api.batch.BatchRuntimeContext;
 import co.cask.cdap.etl.api.batch.BatchSourceContext;
 import co.cask.hydrator.common.Constants;
+import co.cask.hydrator.common.RecordConverter;
 import co.cask.hydrator.common.ReferenceBatchSource;
 import co.cask.hydrator.common.ReferencePluginConfig;
 import co.cask.hydrator.common.SourceInputFormatProvider;
@@ -49,34 +51,46 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nullable;
 
 /**
- * A {@link BigQuerySource} that reads from Google Bigquery.
+ * A {@link BigQuerySource} that reads from Google BigQuery.
  */
 @Plugin(type = "batchsource")
 @Name("BigQuery")
-@Description("Reads from a database table(s) using a configurable BigQuery." +
-  " Outputs one record for each row returned by the query.")
+@Description("Reads from BigQuery tables specified by a configurable BigQuery. " +
+  "Outputs one record for each row returned by the query.")
 public class BigQuerySource extends ReferenceBatchSource<LongWritable, Text, StructuredRecord> {
-  private final BQSourceConfig sourceConfig;
-  private static final String MRBQ_JSON_KEY = "mapred.bq.auth.service.account.json.keyfile";
-  private static final String FSGS_JSON_KEY = "google.cloud.auth.service.account.json.keyfile";
+  private static final String MAPREDUCE_BIGQUERY_JSON_KEY = "mapred.bq.auth.service.account.json.keyfile";
+  private static final String GCS_ACCOUNT_JSON_KEY = "google.cloud.auth.service.account.json.keyfile";
   private static final Gson GSON = new Gson();
   private static final Type MAP_STRING_STRING_TYPE = new TypeToken<Map<String, String>>() { }.getType();
-  private Map<String, String> outputSchemaMapping = new HashMap<>();
-  private  Schema outputSchema;
+
+  private final BQSourceConfig sourceConfig;
+  private final Map<String, String> outputSchemaMapping = new HashMap<>();
+  private static Schema outputSchema;
+
   public BigQuerySource(BQSourceConfig config) {
     super(new ReferencePluginConfig(config.referenceName));
     this.sourceConfig = config;
   }
 
   @Override
-  public void initialize(BatchRuntimeContext context) throws Exception {
+  public void configurePipeline(PipelineConfigurer pipelineConfigurer) {
     init();
+    getOutputSchema();
+    pipelineConfigurer.getStageConfigurer().setOutputSchema(outputSchema);
+  }
+
+  @Override
+  public void initialize(BatchRuntimeContext context) throws Exception {
+    if (outputSchemaMapping.isEmpty()) {
+      init();
+    }
   }
 
   private void init() {
@@ -88,119 +102,137 @@ public class BigQuerySource extends ReferenceBatchSource<LongWritable, Text, Str
   }
 
   @Override
-  public void configurePipeline(PipelineConfigurer pipelineConfigurer) {
-    super.configurePipeline(pipelineConfigurer);
-    init();
-    getOutputSchema();
-    pipelineConfigurer.getStageConfigurer().setOutputSchema(outputSchema);
-  }
-
-  @Override
   public void prepareRun(BatchSourceContext context) throws IOException, GeneralSecurityException,
     ClassNotFoundException, InterruptedException {
     Job job = JobUtils.createInstance();
     Configuration conf = job.getConfiguration();
-
-    conf.set(MRBQ_JSON_KEY, sourceConfig.jsonFilePath);
-    conf.set(FSGS_JSON_KEY, sourceConfig.jsonFilePath);
+    conf.set(MAPREDUCE_BIGQUERY_JSON_KEY, sourceConfig.jsonFilePath);
+    conf.set(GCS_ACCOUNT_JSON_KEY, sourceConfig.jsonFilePath);
     conf.set(BigQueryConfiguration.PROJECT_ID_KEY, sourceConfig.projectId);
     if (sourceConfig.importQuery != null) {
       conf.set(BigQueryConfiguration.INPUT_QUERY_KEY, sourceConfig.importQuery);
     }
-      conf.set(BigQueryConfiguration.TEMP_GCS_PATH_KEY, sourceConfig.tmpBucketPath);
+    conf.set(BigQueryConfiguration.TEMP_GCS_PATH_KEY, sourceConfig.tmpBucketPath);
     conf.set("fs.gs.project.id", sourceConfig.projectId);
-    BigQueryConfiguration.configureBigQueryInput(conf, sourceConfig.fullyQualifiedInputTableId);
+    BigQueryConfiguration.configureBigQueryInput(conf, sourceConfig.inputTable);
     job.setOutputKeyClass(LongWritable.class);
     job.setOutputValueClass(Text.class);
     context.setInput(Input.of(sourceConfig.referenceName,
                               new SourceInputFormatProvider(JsonTextBigQueryInputFormat.class, conf)));
   }
 
-
   @Override
   public void transform(KeyValue<LongWritable, Text> input, Emitter<StructuredRecord> emitter) throws IOException {
     getOutputSchema();
-    StructuredRecord record = jsonTransform(input.getValue());
+    StructuredRecord record = convertToStructuredRecord(input.getValue());
     emitter.emit(record);
-
   }
 
-  private StructuredRecord jsonTransform(Text jsonText) {
-    Map<String, String> map;
-    map = GSON.fromJson(jsonText.toString(), MAP_STRING_STRING_TYPE);
+  private StructuredRecord convertToStructuredRecord(Text jsonText) {
+    Map<String, String> map = GSON.fromJson(jsonText.toString(), MAP_STRING_STRING_TYPE);
     StructuredRecord.Builder builder = StructuredRecord.builder(outputSchema);
     for (Schema.Field field : outputSchema.getFields()) {
       String fieldName = field.getName();
       Schema schema = field.getSchema();
-      if (schema.getType() == Schema.Type.INT) {
-        builder.set(fieldName, Integer.parseInt(map.get(fieldName)));
-      } else {
-        builder.set(fieldName, map.get(fieldName));
+      Schema.Type fieldType = schema.getType();
+      switch (fieldType) {
+        case INT:
+          builder.set(fieldName, Integer.parseInt(map.get(fieldName)));
+        case BYTES:
+          builder.set(fieldName, Byte.parseByte(map.get(fieldName)));
+        case LONG:
+          builder.set(fieldName, Long.parseLong(map.get(fieldName)));
+        case FLOAT:
+          builder.set(fieldName, Float.parseFloat(map.get(fieldName)));
+        case DOUBLE:
+          builder.set(fieldName, Double.parseDouble(map.get(fieldName)));
+        case BOOLEAN:
+          builder.set(fieldName, Boolean.parseBoolean(map.get(fieldName)));
+        case NULL:
+          builder.set(fieldName, null);
+        case STRING:
+          builder.set(fieldName, map.get(fieldName));
+        default:
+          throw new UnexpectedFormatException("field type " + fieldType + " is not supported.");
       }
     }
     return builder.build();
+  }
+
+  protected Object convertField(Object field, Schema fieldSchema) throws IOException {
+    Schema.Type fieldType = fieldSchema.getType();
+    switch (fieldType) {
+      case NULL:
+        return null;
+      case STRING:
+        return field.toString();
+      case BYTES:
+      case INT:
+      case LONG:
+      case FLOAT:
+      case DOUBLE:
+      case BOOLEAN:
+        return field;
+      default:
+        throw new UnexpectedFormatException("field type " + fieldType + " is not supported.");
+    }
   }
 
   /**
    * {@link PluginConfig} class for {@link BigQuerySource}
    */
   public static class BQSourceConfig extends PluginConfig {
-    private static final String IMPORT_QUERY = "importQuery";
-    private static final String PROJECT_ID = "projectId";
-    private static final String INPUT_TABLE_ID = "InputTableId";
-    private static final String JSON_FILE_PATH = "jsonFilePath";
-    private static final String TEMP_BUCKET = "tempBuketPath";
-    private static final String OUTPUT_SCHEMA = "outputSchema";
-    private static final String PROJECTID_DESC = "The ID of the project in Google Cloud";
-    private static final String TEMP_BUCKET_DESC = "the tempory google cloud storage directory to store the " +
-                                                   "intermediate result. Example: gs://bucketname/directoryname, " +
-                                                   "the directory should not be existed. User should delete this " +
-                                                   "directory afterward manually to avoid extra google storage charge.";
+    private static final String PROJECTID_DESC = "The ID of the project in Google Cloud Platform";
+    private static final String TEMP_BUCKET_DESC = "The temporary Google Cloud Storage directory to be used for " +
+      "storing the intermediate results. " +
+      "e.g. 'gs://bucketname/directoryname'. The directory should not " +
+      "already exist. Users should manually delete this directory " +
+      "afterwards to avoid any extra Google Storage charges.";
     private static final String IMPORT_QUERY_DESC = "The SELECT query to use to import data from the specified table." +
-                                                    " example: SELECT TOP(corpus, 10) as title, COUNT(*) as " +
-                                                    "unique_words FROM [publicdata:samples.shakespeare], where " +
-                                                    "publicdata is the project name, smalples is the dataset name, " +
-                                                    "shakespare is the table name. This is optional, if empty, just " +
-                                                    "read the inputTable configured";
-    private static final String JSON_KEYFILE_DESC = "the credential json key file path";
+      " For example: 'SELECT TOP(corpus, 10) as title, COUNT(*) as " +
+      "unique_words FROM publicdata:samples.shakespeare', where " +
+      "'publicdata' is the project name (optional), 'samples' is the " +
+      "dataset name, and 'shakespare' is the table name. If this query " +
+      "is not provided, reads instead from the configured inputTable.";
+    private static final String JSON_KEYFILE_DESC = "JSON key file path for credentials, Note that user should " +
+      "upload his credential file to the same path of all nodes.";
     private static final String INPUT_TABLE_DESC = "The BigQuery table to read from, in the form " +
-                                                   "[optional projectId]:[datasetId].[tableId]. Example: " +
-                                                   "publicdata:samples.shakespeare. Note that if the import query is " +
-                                                   "specified, this table should be a empty table with the " +
-                                                   "query result schema. User need to first create such a table";
-    private static final String OUTPUTSCHEMA_DESC = "Comma separated mapping of column names in the output schema to " +
-                                                   "the data types; for example: 'A:string,B:int'";
+      "'<projectId (optional)>:<datasetId>.<tableId>'. The 'projectId' " +
+      "is optional. Example: 'publicdata:samples.shakespeare'. Note: if " +
+      "the import query is specified, this table should already exist " +
+      "and be an empty table with the query result schema.";
+    private static final String OUTPUTSCHEMA_DESC = "Comma-separated mapping of output schema column names to " +
+      "data types; for example: 'A:string,B:int'.";
     @Name(Constants.Reference.REFERENCE_NAME)
     @Description(Constants.Reference.REFERENCE_NAME_DESCRIPTION)
-    public String referenceName;
+    String referenceName;
 
-    @Name(IMPORT_QUERY)
+    @Name("importQuery")
     @Description(IMPORT_QUERY_DESC)
     @Nullable
     @Macro
     String importQuery;
 
-    @Name(PROJECT_ID)
+    @Name("projectId")
     @Description(PROJECTID_DESC)
     @Macro
     String projectId;
 
-    @Name(OUTPUT_SCHEMA)
+    @Name("outputSchema")
     @Description(OUTPUTSCHEMA_DESC)
-    @Macro
     private String outputSchema;
 
-    @Name(INPUT_TABLE_ID)
+    @Name("inputTableId")
     @Description(INPUT_TABLE_DESC)
     @Macro
-    String fullyQualifiedInputTableId;
+    String inputTable;
 
-    @Name(JSON_FILE_PATH)
+    @Name("jsonFilePath")
     @Description(JSON_KEYFILE_DESC)
     @Macro
     String jsonFilePath;
 
-    @Name(TEMP_BUCKET)
+    @Name("tempBucketPath")
     @Description(TEMP_BUCKET_DESC)
     @Macro
     String tmpBucketPath;
@@ -208,19 +240,17 @@ public class BigQuerySource extends ReferenceBatchSource<LongWritable, Text, Str
 
   private void getOutputSchema() {
     if (outputSchema == null) {
-      List<Schema.Field> outputFields = Lists.newArrayList();
+      List<Schema.Field> outputFields = new ArrayList<>();
       try {
         for (String fieldName : outputSchemaMapping.keySet()) {
           Schema fieldType = Schema.of(Schema.Type.valueOf(outputSchemaMapping.get(fieldName).toUpperCase()));
           outputFields.add(Schema.Field.of(fieldName, fieldType));
         }
-
       } catch (Exception e) {
-        throw new IllegalArgumentException("Exception while creating output schema " +
-                                             "Invalid output " + "schema: " + e.getMessage(), e);
+        throw new IllegalArgumentException("Error while parsing output schema: invalid output schema " +
+                                             e.getMessage(), e);
       }
       outputSchema = Schema.recordOf("outputSchema", outputFields);
     }
   }
-
 }
