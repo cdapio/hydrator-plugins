24ebd6fb99f5cb8c14a23cdab969ac86799a2a78
Russ Savage <russ@cask.co>
2016-11-15 22:05:24 -0800

    adding field appender plugin

---
+++ transform-plugins/src/test/java/co/cask/hydrator/plugin/FieldAppenderTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.hydrator.plugin;
+
+import co.cask.cdap.api.data.format.StructuredRecord;
+import co.cask.cdap.api.data.schema.Schema;
+import co.cask.cdap.etl.api.Transform;
+import co.cask.hydrator.common.test.MockEmitter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Tests {@link FieldAppender}.
+ */
+public class FieldAppenderTest {
+  private static final Schema INPUT = Schema.recordOf("input",
+                                                      Schema.Field.of("a", Schema.of(Schema.Type.STRING)));
+
+  @Test
+  public void testIdGenerator() throws Exception {
+    Schema output = Schema.recordOf("output",
+                                    Schema.Field.of("a", Schema.of(Schema.Type.STRING)),
+                                    Schema.Field.of("b", Schema.of(Schema.Type.STRING)));
+    FieldAppender.Config config = new FieldAppender.Config("b\t${batch-id}-~uuid~", output.toString());
+    Transform<StructuredRecord, StructuredRecord> transform = new FieldAppender(config);
+    transform.initialize(null);
+
+    MockEmitter<StructuredRecord> emitter = new MockEmitter<>();
+    transform.transform(StructuredRecord.builder(INPUT).set("a", "cdap").build(), emitter);
+    Assert.assertNotNull(emitter.getEmitted().get(0).get("b"));
+  }
+
+  @Test
+  public void testDateSubstitution() throws Exception {
+    Schema output = Schema.recordOf("output",
+                                    Schema.Field.of("a", Schema.of(Schema.Type.STRING)),
+                                    Schema.Field.of("b", Schema.of(Schema.Type.STRING)));
+    FieldAppender.Config config = new FieldAppender.Config("b\t~datetime:yyyy-MM-dd~", output.toString());
+    Transform<StructuredRecord, StructuredRecord> transform = new FieldAppender(config);
+    transform.initialize(null);
+
+    MockEmitter<StructuredRecord> emitter = new MockEmitter<>();
+    transform.transform(StructuredRecord.builder(INPUT).set("a", "cdap").build(), emitter);
+    Assert.assertNotNull(emitter.getEmitted().get(0).get("b"));
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    Assert.assertEquals(sdf.format(System.currentTimeMillis()), emitter.getEmitted().get(0).get("b"));
+  }
+
+  @Test
+  public void testFieldSubstitution() throws Exception {
+    Schema output = Schema.recordOf("output",
+                                    Schema.Field.of("a", Schema.of(Schema.Type.STRING)),
+                                    Schema.Field.of("b", Schema.of(Schema.Type.STRING)));
+    FieldAppender.Config config = new FieldAppender.Config("b\t~field:a~-~field:a~", output.toString());
+    Transform<StructuredRecord, StructuredRecord> transform = new FieldAppender(config);
+    transform.initialize(null);
+
+    MockEmitter<StructuredRecord> emitter = new MockEmitter<>();
+    transform.transform(StructuredRecord.builder(INPUT).set("a", "cdap").build(), emitter);
+    Assert.assertNotNull(emitter.getEmitted().get(0).get("b"));
+    Assert.assertEquals("cdap-cdap", emitter.getEmitted().get(0).get("b"));
+  }
+}
