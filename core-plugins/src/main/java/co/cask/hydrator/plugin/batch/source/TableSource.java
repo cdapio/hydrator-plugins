a3eedddf89a89f368f99554771b32d7d6ff01197
Sheivin Goyal <sheivingoyal@Sheivins-MacBook-Pro.local>
2017-05-17 16:03:02 -0700

    Table names for source and sink will be same as dataset name

--- core-plugins/src/main/java/co/cask/hydrator/plugin/batch/source/TableSource.java
+++ core-plugins/src/main/java/co/cask/hydrator/plugin/batch/source/TableSource.java
@@ -35,7 +35,6 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
