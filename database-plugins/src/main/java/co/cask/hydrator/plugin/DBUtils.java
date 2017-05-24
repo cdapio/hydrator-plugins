17f2aa3f8ca66888cf93f426dc384888bca08148
Rohit Sinha <rsinha@cask.co>
2017-05-23 20:15:48 -0700

    Move transformValue function from DBRecord to DBUtils as public static method

--- database-plugins/src/main/java/co/cask/hydrator/plugin/DBUtils.java
+++ database-plugins/src/main/java/co/cask/hydrator/plugin/DBUtils.java
@@ -23,10 +23,14 @@ import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.Clob;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -228,6 +232,57 @@ public final class DBUtils {
     }
 
     return type;
+  }
+
+  @Nullable
+  public static Object transformValue(int sqlColumnType, ResultSet resultSet, String fieldName) throws SQLException {
+    Object original = resultSet.getObject(fieldName);
+    if (original != null) {
+      switch (sqlColumnType) {
+        case Types.SMALLINT:
+        case Types.TINYINT:
+          return ((Number) original).intValue();
+        case Types.NUMERIC:
+        case Types.DECIMAL:
+          return ((BigDecimal) original).doubleValue();
+        case Types.DATE:
+          return resultSet.getDate(fieldName).getTime();
+        case Types.TIME:
+          return resultSet.getTime(fieldName).getTime();
+        case Types.TIMESTAMP:
+          return resultSet.getTimestamp(fieldName).getTime();
+        case Types.BLOB:
+          Object toReturn;
+          Blob blob = (Blob) original;
+          try {
+            toReturn = blob.getBytes(1, (int) blob.length());
+          } finally {
+            blob.free();
+          }
+          return toReturn;
+        case Types.CLOB:
+          String s;
+          StringBuilder sbf = new StringBuilder();
+          Clob clob = (Clob) original;
+          try {
+            try (BufferedReader br = new BufferedReader(clob.getCharacterStream(1, (int) clob.length()))) {
+              if ((s = br.readLine()) != null) {
+                sbf.append(s);
+              }
+              while ((s = br.readLine()) != null) {
+                sbf.append(System.getProperty("line.separator"));
+                sbf.append(s);
+              }
+            }
+          } catch (IOException e) {
+            throw new SQLException(e);
+          } finally {
+            clob.free();
+          }
+          return sbf.toString();
+      }
+    }
+    return original;
   }
 
   /**
