e381b4b81e29653df72804976be2c8939a8ff69a
Sheivin Goyal <sheivingoyal@Sheivins-MacBook-Pro.local>
2017-07-24 15:25:25 -0700

    Rebasing after fixing widget file

--- core-plugins/src/main/java/co/cask/hydrator/plugin/batch/source/PathTrackingInputFormat.java
+++ core-plugins/src/main/java/co/cask/hydrator/plugin/batch/source/PathTrackingInputFormat.java
@@ -145,7 +145,6 @@ public class PathTrackingInputFormat extends FileInputFormat<NullWritable, Struc
 
     @Override
     public void close() throws IOException {
-      System.out.println("* * * ** * * * * " + schema.toString());
       delegate.close();
     }
 
