a5dbe96d5f5dd2bc32e1cc56a1676c1cbe76effb
vinisha <vinishashah@google.com>
2019-09-11 15:07:50 -0700

    Update IdUtil to use failure collector

--- hydrator-common/src/main/java/io/cdap/plugin/common/IdUtils.java
+++ hydrator-common/src/main/java/io/cdap/plugin/common/IdUtils.java
@@ -16,6 +16,8 @@
 
 package io.cdap.plugin.common;
 
+import io.cdap.cdap.etl.api.FailureCollector;
+
 import java.util.regex.Pattern;
 
 /**
@@ -28,10 +30,32 @@ public final class IdUtils {
 
   private static final Pattern datasetIdPattern = Pattern.compile("[$\\.a-zA-Z0-9_-]+");
 
+  /**
+   * Validates provided id. Valid id contains letters, numbers, '_' , '-' , '.' or '$'.
+   *
+   * @param id id to be validated
+   * @throws IllegalArgumentException if id is invalid
+   */
   public static void validateId(String id) throws IllegalArgumentException {
     if (!datasetIdPattern.matcher(id).matches()) {
-      throw new IllegalArgumentException(String.format("%s is not a valid id. Allowed characters are letters, " +
-                                                         "numbers, and _, -, ., or $.", id));
+      throw new IllegalArgumentException(
+        String.format("Invalid id '%s'. Supported characters are: letters, numbers, and '_', '-', '.', or '$'.", id));
+    }
+  }
+
+  /**
+   * Validates provided reference name. If the reference name is not valid,
+   * adds the validation failure to the failure collector.
+   * Valid reference name contains letters, numbers, '_' , '-' , '.' or '$'.
+   *
+   * @param referenceName reference name to be validated
+   * @param collector failure collector
+   */
+  public static void validateReferenceName(String referenceName, FailureCollector collector) {
+    if (!datasetIdPattern.matcher(referenceName).matches()) {
+      collector.addFailure(String.format("Invalid reference name '%s'.", referenceName),
+                           "Supported characters are: letters, numbers, and '_', '-', '.', or '$'.")
+        .withConfigProperty(Constants.Reference.REFERENCE_NAME);
     }
   }
 }
