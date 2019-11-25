5673663a27ba63bec59f5d619f44673f59c37234
Lav Mudgal <lav.mudgal@guavus.com>
2019-11-25 13:22:59 +0530

    added parquet & orc getSchema in file-batch-source

--- format-common/src/main/java/co/cask/hydrator/format/input/OrcInputProvider.java
+++ format-common/src/main/java/co/cask/hydrator/format/input/OrcInputProvider.java
@@ -17,6 +17,15 @@
 package co.cask.hydrator.format.input;
 
 import co.cask.cdap.api.data.schema.Schema;
+import co.cask.hydrator.format.OrcToStructuredTransformer;
+import com.google.common.base.Strings;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.orc.OrcFile;
+import org.apache.orc.Reader;
+import org.apache.orc.TypeDescription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import javax.annotation.Nullable;
@@ -26,14 +35,28 @@ import javax.annotation.Nullable;
  */
 public class OrcInputProvider implements FileInputFormatterProvider {
 
-  @Nullable
-  @Override
-  public Schema getSchema(@Nullable String pathField) {
-    return null;
-  }
+    private static final Logger LOG = LoggerFactory.getLogger(OrcInputProvider.class);
 
-  @Override
-  public FileInputFormatter create(Map<String, String> properties, @Nullable Schema schema) {
-    return new OrcInputFormatter(schema);
-  }
+    @Nullable
+    @Override
+    public Schema getSchema(@Nullable String pathField) {
+        Configuration configuration = new Configuration();
+        if (Strings.isNullOrEmpty(pathField)) {
+            throw new IllegalArgumentException("Path is a required field for fetching Schema");
+        }
+        Path path = new Path(pathField);
+        try {
+            Reader reader = OrcFile.createReader(path, new OrcFile.ReaderOptions(configuration));
+            TypeDescription typeDescription = reader.getSchema();
+            return OrcToStructuredTransformer.convertSchema(typeDescription);
+        } catch (Exception e) {
+            LOG.error("Error in fetching orc schema => " + e.getMessage(), e);
+            throw new RuntimeException("Error in fetching orc schema => " + e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public FileInputFormatter create(Map<String, String> properties, @Nullable Schema schema) {
+        return new OrcInputFormatter(schema);
+    }
 }
--- format-common/src/main/java/co/cask/hydrator/format/input/ParquetInputProvider.java
+++ format-common/src/main/java/co/cask/hydrator/format/input/ParquetInputProvider.java
@@ -17,6 +17,17 @@
 package co.cask.hydrator.format.input;
 
 import co.cask.cdap.api.data.schema.Schema;
+import co.cask.hydrator.format.AvroSchemaConverter;
+import com.google.common.base.Strings;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
+import org.apache.parquet.format.converter.ParquetMetadataConverter;
+import org.apache.parquet.hadoop.ParquetFileReader;
+import org.apache.parquet.hadoop.metadata.ParquetMetadata;
+import org.apache.parquet.schema.MessageType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import javax.annotation.Nullable;
@@ -26,14 +37,31 @@ import javax.annotation.Nullable;
  */
 public class ParquetInputProvider implements FileInputFormatterProvider {
 
-  @Nullable
-  @Override
-  public Schema getSchema(@Nullable String pathField) {
-    return null;
-  }
+    private static final Logger LOG = LoggerFactory.getLogger(ParquetInputProvider.class);
 
-  @Override
-  public FileInputFormatter create(Map<String, String> properties, @Nullable Schema schema) {
-    return new ParquetInputFormatter(schema);
-  }
+    @Nullable
+    @Override
+    public Schema getSchema(@Nullable String pathField) {
+        if (Strings.isNullOrEmpty(pathField)) {
+            throw new IllegalArgumentException("Path Field should contain a valid path for fetching Schema");
+        }
+        Path path = new Path(pathField);
+        try {
+            Configuration conf = new Configuration();
+            conf.setBoolean(AvroSchemaConverter.ADD_LIST_ELEMENT_RECORDS, false);
+            ParquetMetadata readFooter = ParquetFileReader.readFooter(conf, path, ParquetMetadataConverter.NO_FILTER);
+            MessageType mt = readFooter.getFileMetaData().getSchema();
+            org.apache.avro.Schema avroSchema = new AvroSchemaConverter(conf).convert(mt);
+            return Schema.parseJson(avroSchema.toString());
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            throw new RuntimeException("Error in reading parquet schema => " + e.getMessage(), e);
+        }
+    }
+
+
+    @Override
+    public FileInputFormatter create(Map<String, String> properties, @Nullable Schema schema) {
+        return new ParquetInputFormatter(schema);
+    }
 }
