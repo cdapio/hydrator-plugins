13f63f1686efa91fcb1977fb5bb506d040623a4c
Nitin Motgi <nmotgi@google.com>
2020-04-06 08:16:23 -0700

    [CDAP-16532] Addressed review comments, defaulted format to JSON as in UI as well changed to use streams

--- format-common/src/main/java/io/cdap/plugin/format/plugin/AbstractFileSinkConfig.java
+++ format-common/src/main/java/io/cdap/plugin/format/plugin/AbstractFileSinkConfig.java
@@ -85,7 +85,7 @@ public abstract class AbstractFileSinkConfig extends PluginConfig implements Fil
       }
     }
 
-    FileFormat format = FileFormat.CSV;
+    FileFormat format = FileFormat.JSON;
     try {
       format = getFormat();
     } catch (IllegalArgumentException e) {
@@ -97,18 +97,16 @@ public abstract class AbstractFileSinkConfig extends PluginConfig implements Fil
       // If there are non-simple field types, then serializing as CSV might not be appropriate.
       // This restriction is on Csv, Delimited and Tab separated files.
       if (format == FileFormat.CSV || format == FileFormat.DELIMITED || format == FileFormat.TSV) {
-        boolean simpleFields = true;
-        for (Schema.Field field : schema.getFields()) {
-          if (!field.getSchema().isSimpleOrNullableSimple()) {
-            simpleFields = false;
-            break;
-          }
+        boolean allSimpleFields = schema.getFields().stream()
+          .map(Schema.Field::getSchema)
+          .allMatch(Schema::isSimpleOrNullableSimple);
+        if (allSimpleFields == false) {
+          collector.addFailure(
+            String.format("Input has multi-level structure that cannot be represented appropriately in %s.",
+                          format.name()),
+            "Consider using json, avro or parquet to write data."
+          ).withConfigProperty(NAME_FORMAT);
         }
-        collector.addFailure(
-          String.format("Input has multi-level structure that cannot be represented appropriately in %s.",
-                        format.name()),
-          "Consider using json, avro or parquet to write data."
-        ).withConfigProperty(NAME_FORMAT);
       }
     } catch (IllegalArgumentException e) {
       collector.addFailure(e.getMessage(), null).withConfigProperty(NAME_SCHEMA).withStacktrace(e.getStackTrace());
