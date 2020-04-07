c9dcf7291a757d7acc3535f24090fc0c90fd0ae6
Venudhar Ravishankar <vravish@google.com>
2020-04-06 23:01:22 -0700

    [CDAP-16290] Added recordLineage method to ReferenceBatchSink and ReferenceBatchSource so all subclasses plugins can use it

--- hydrator-common/src/main/java/io/cdap/plugin/common/ReferenceBatchSink.java
+++ hydrator-common/src/main/java/io/cdap/plugin/common/ReferenceBatchSink.java
@@ -16,9 +16,16 @@
 
 package io.cdap.plugin.common;
 
+import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.cdap.etl.api.FailureCollector;
 import io.cdap.cdap.etl.api.PipelineConfigurer;
 import io.cdap.cdap.etl.api.batch.BatchSink;
+import io.cdap.cdap.etl.api.batch.BatchSinkContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A {@link BatchSink} that verifies referenceName property
@@ -28,6 +35,8 @@ import io.cdap.cdap.etl.api.batch.BatchSink;
  * @param <VAL_OUT> the type of value the sink outputs
  */
 public abstract class ReferenceBatchSink<IN, KEY_OUT, VAL_OUT> extends BatchSink<IN, KEY_OUT, VAL_OUT> {
+  private static final Logger LOG = LoggerFactory.getLogger(ReferenceBatchSink.class);
+
   private final ReferencePluginConfig config;
 
   public ReferenceBatchSink(ReferencePluginConfig config) {
@@ -40,5 +49,34 @@ public abstract class ReferenceBatchSink<IN, KEY_OUT, VAL_OUT> extends BatchSink
     FailureCollector collector = pipelineConfigurer.getStageConfigurer().getFailureCollector();
     IdUtils.validateReferenceName(config.referenceName, collector);
     collector.getOrThrowException();
+  }
+
+  /**
+   * Record field-level lineage for batch sink plugins (WriteOperation). This method should be called from prepareRun of
+   * any batch sink plugin.
+   *
+   * @param context BatchSinkContext from prepareRun
+   * @param outputName name of output dataset
+   * @param tableSchema schema of fields. Also used to determine list of field names. Schema and schema.getFields() must
+   * not be null.
+   * @param operationName name of the operation
+   * @param description operation description; complete sentences preferred
+   */
+  protected void recordLineage(BatchSinkContext context, String outputName, Schema tableSchema, String operationName,
+                               String description) {
+    if (tableSchema == null) {
+      LOG.warn("Schema for output %s is null. Field-level lineage will not be recorded.", outputName);
+      return;
+    }
+    if (tableSchema.getFields() == null) {
+      LOG.warn("Schema fields for output %s is empty. Field-level lineage will not be recorded", outputName);
+      return;
+    }
+    LineageRecorder lineageRecorder = new LineageRecorder(context, outputName);
+    lineageRecorder.createExternalDataset(tableSchema);
+    List<String> fieldNames = tableSchema.getFields().stream().map(Schema.Field::getName).collect(Collectors.toList());
+    if (!fieldNames.isEmpty()) {
+      lineageRecorder.recordWrite(operationName, description, fieldNames);
+    }
   }
 }
