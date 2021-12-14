39c812b0615c4555f5adb6e187d3bed0729ccbdb
Pritam Sawant <sawantp@google.com>
2021-12-14 11:47:33 +0530

    Review comments

--- e2e-test/src/test/java/io/cdap/plugin/utils/GCConstants.java
+++ e2e-test/src/test/java/io/cdap/plugin/utils/GCConstants.java
@@ -1,25 +1,27 @@
+/*
+ * Copyright © 2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.cdap.plugin.utils;
 
 /**
- * constants.
+ * E2E test constants.
  */
 public class GCConstants {
   public static final String ERROR_MSG_COLOR = "errorMessageColor";
-  public static final String ERROR_MSG_DATASET = "errorMessageDataset";
-  public static final String ERROR_MSG_TEMP_BUCKET = "errorMessageTemporaryBucket";
-  public static final String ERROR_MSG_REF = "errorMessageReference";
-  public static final String CDF_ATHENA_BQMTABLE = "cdf-athena-bq-bq-multitable";
-  public static final String A = "a";
-  public static final String COMMENT_BQMT = "COMMENT_BQMT";
-  public static final String BQMT_LABEL = "BQMT_Labelname";
-  public static final String INVALID_TESTDATA = "#@#@#@#@#@";
-  public static final String DELIMITED = "delimited";
-  public static final String TABLE_DEL_MSG = "Table Deleted Successfully";
   public static final String ERROR_MSG_MANDATORY = "errorMessageMandatory";
   public static final String ERROR_MSG_VALIDATION = "errorMessageValidation";
-  public static final String ERROR_MSG_ERROR_FOUND_VALIDATION = "errorMessageErrorFoundValidation";
-  public static final String ERROR_MSG_INCORRECT_TABLE = "errorMessageTable";
-  public static final String ERROR_MSG_GCS_INVALID_PATH = "errorMessageGCSInvalidPath";
-  public static final String ERROR_MSG_GCS_INVALID_BUCKET_NAME = "errorMessageGCSInvalidBucketName";
+  public static final String ERROR_MSG_ERROR_FOUND_VALIDATION = "errorMessageErrorFoundValidation";;
   public static final String ERROR_MSG_FILE_INVALID_OUTPUTFIELD = "errorMessageFileInvalidOutputField";
 }
