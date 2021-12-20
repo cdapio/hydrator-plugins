f08ec620e81d11530c8ec2223f0663cf0a1c7bdf
Pritam Sawant <sawantp@google.com>
2021-12-20 20:31:05 +0530

    review comments

--- e2e-test/src/test/java/io/cdap/plugin/utils/E2ETestConstants.java
+++ e2e-test/src/test/java/io/cdap/plugin/utils/E2ETestConstants.java
@@ -18,7 +18,7 @@ package io.cdap.plugin.utils;
 /**
  * E2E test constants.
  */
-public class GCConstants {
+public class E2ETestConstants {
   public static final String ERROR_MSG_COLOR = "errorMessageColor";
   public static final String ERROR_MSG_MANDATORY = "errorMessageMandatory";
   public static final String ERROR_MSG_VALIDATION = "errorMessageValidation";
