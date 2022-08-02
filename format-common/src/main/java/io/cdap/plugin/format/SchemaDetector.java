88a607da63a51273ccdc80d0b9f8de27dd274ddd
albertshau <ashau@google.com>
2022-08-02 14:00:16 -0700

    CDAP-19461 fix schema detection with regex
    Fix schema detection so that it properly uses the regex config
    to filter out files that should be ignored.

--- format-common/src/main/java/io/cdap/plugin/format/SchemaDetector.java
+++ format-common/src/main/java/io/cdap/plugin/format/SchemaDetector.java
@@ -25,12 +25,16 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.mapreduce.Job;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
 /**
@@ -50,20 +54,38 @@ public class SchemaDetector {
    * @param formatContext format context
    * @param fileSystemProperties any properties that need to be set in order to read from the Hadoop FileSystem
    * @return the schema of files at the path, or null if the schema is not known.
-   * @throws IOException
+   * @throws IOException if there was an exception interacting with the filesystem
    */
   @Nullable
   public Schema detectSchema(String path, FormatContext formatContext,
+                             Map<String, String> fileSystemProperties) throws IOException {
+    return detectSchema(path, null, formatContext, fileSystemProperties);
+  }
+
+  /**
+   * Detect the schema for files at a specified path, where files need to match the specified pattern.
+   *
+   * @param path path to the files. Can be a file or a directory
+   * @param pattern pattern that the files much match in order to be considered for schema detection. Files that do not
+   *                match this pattern will be skipped. If none is given, any file can be used for schema detection.
+   * @param formatContext format context
+   * @param fileSystemProperties any properties that need to be set in order to read from the Hadoop FileSystem
+   * @return the schema of files at the path, or null if the schema is not known.
+   * @throws IOException if there was an exception interacting with the filesystem
+   */
+  @Nullable
+  public Schema detectSchema(String path, @Nullable Pattern pattern, FormatContext formatContext,
                              Map<String, String> fileSystemProperties) throws IOException {
     if (path == null) {
       return null;
     }
 
-    InputFiles inputFiles = getInputFiles(path, fileSystemProperties);
+    InputFiles inputFiles = getInputFiles(path, pattern, fileSystemProperties);
     return inputFormat.detectSchema(formatContext, inputFiles);
   }
 
-  private InputFiles getInputFiles(String path, Map<String, String> fileSystemProperties) throws IOException {
+  private InputFiles getInputFiles(String path, @Nullable Pattern pattern,
+                                   Map<String, String> fileSystemProperties) throws IOException {
     Job job = JobUtils.createInstance();
     Configuration configuration = job.getConfiguration();
     for (Map.Entry<String, String> entry : fileSystemProperties.entrySet()) {
@@ -73,6 +95,7 @@ public class SchemaDetector {
       configuration.set(entry.getKey(), entry.getValue());
     }
 
+    PathFilter pathFilter = getPathFilter(pattern, configuration);
     Path fsPath = new Path(path);
 
     ClassLoader cl = configuration.getClassLoader();
@@ -80,7 +103,7 @@ public class SchemaDetector {
     FileSystem fs = FileSystem.get(fsPath.toUri(), configuration);
     configuration.setClassLoader(cl);
 
-    if (!fs.exists(fsPath)) {
+    if (!fs.exists(fsPath) || !pathFilter.accept(fsPath)) {
       throw new IOException("Input path not found");
     }
 
@@ -99,6 +122,20 @@ public class SchemaDetector {
       throw new IllegalArgumentException("Provided directory '" + path + "' is empty");
     }
 
-    return new FileSystemInputFiles(fs, Arrays.asList(files));
+    List<FileStatus> filteredFiles = Arrays.stream(files)
+      .filter(fStatus -> pathFilter.accept(fStatus.getPath()))
+      .collect(Collectors.toList());
+
+    return new FileSystemInputFiles(fs, filteredFiles);
+  }
+
+  private PathFilter getPathFilter(@Nullable Pattern pattern, Configuration configuration) {
+    if (pattern == null) {
+      return p -> true;
+    }
+    RegexPathFilter.configure(configuration, pattern);
+    RegexPathFilter regexPathFilter = new RegexPathFilter();
+    regexPathFilter.setConf(configuration);
+    return regexPathFilter;
   }
 }
