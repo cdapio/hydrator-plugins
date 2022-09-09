8558303b282fa815b346e371a8026986d6147044
Pritam Sawant <sawantp@google.com>
2022-09-09 17:53:03 +0530

    e2e File plugin test scenarios

--- core-plugins/src/e2e-test/java/io/cdap/plugin/common/stepsdesign/TestSetupHooks.java
+++ core-plugins/src/e2e-test/java/io/cdap/plugin/common/stepsdesign/TestSetupHooks.java
@@ -51,6 +51,8 @@ public class TestSetupHooks {
     if (firstFileSourceTestFlag) {
       PluginPropertyUtils.addPluginProp("csvFile", Paths.get(TestSetupHooks.class.getResource
         ("/" + PluginPropertyUtils.pluginProp("csvFile")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("csvNoHeaderFile", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("csvNoHeaderFile")).getPath()).toString());
       PluginPropertyUtils.addPluginProp("csvAllDataTypeFile", Paths.get(TestSetupHooks.class.getResource
         ("/" + PluginPropertyUtils.pluginProp("csvAllDataTypeFile")).getPath()).toString());
       PluginPropertyUtils.addPluginProp("tsvFile", Paths.get(TestSetupHooks.class.getResource
@@ -132,6 +134,32 @@ public class TestSetupHooks {
         ("/" + PluginPropertyUtils.pluginProp("errorCollectorDefaultConfigOutput")).getPath()).toString());
       PluginPropertyUtils.addPluginProp("errorCollectorCustomConfigOutput", Paths.get(TestSetupHooks.class.getResource
         ("/" + PluginPropertyUtils.pluginProp("errorCollectorCustomConfigOutput")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("outputForAllDataTypeTest", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("outputForAllDataTypeTest")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("outputForTsvInputTest", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("outputForTsvInputTest")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("outputForOutputFieldTest", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("outputForOutputFieldTest")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("outputForOverrideTest", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("outputForOverrideTest")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("outputForDelimitedTest", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("outputForDelimitedTest")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("outputForRegexTest", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("outputForRegexTest")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("outputForRecursiveTest", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("outputForRecursiveTest")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("outputForCSVInputTest", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("outputForCSVInputTest")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("outputForTextInputTest", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("outputForTextInputTest")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("outputForCSVDelimitedOutputTest", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("outputForCSVDelimitedOutputTest")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("tsvFormatOutput", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("tsvFormatOutput")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("jsonFormatOutput", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("jsonFormatOutput")).getPath()).toString());
+      PluginPropertyUtils.addPluginProp("orcFormatOutput", Paths.get(TestSetupHooks.class.getResource
+        ("/" + PluginPropertyUtils.pluginProp("orcFormatOutput")).getPath()).toString());
       fileSinkOutputFolder = PluginPropertyUtils.pluginProp("filePluginOutputFolder");
       firstFileSinkTestFlag = false;
     }
---
+++ core-plugins/src/e2e-test/java/io/cdap/plugin/file/stepsdesign/FilePlugin.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.cdap.plugin.file.stepsdesign;
+
+import io.cdap.e2e.pages.actions.CdfPluginPropertiesActions;
+import io.cdap.e2e.utils.CdfHelper;
+import io.cdap.e2e.utils.PluginPropertyUtils;
+import io.cdap.plugin.file.actions.FileActions;
+import io.cdap.plugin.utils.E2ETestConstants;
+import io.cucumber.java.en.Then;
+
+/**
+ * File plugin related Step design.
+ **/
+
+public class FilePlugin implements CdfHelper {
+
+  @Then("Enter File plugin property: override field name with value: {string}")
+  public void enterFilePluginPropertyOverrideFieldNameWithValue(String fieldName) {
+    FileActions.enterOverrideFieldName(PluginPropertyUtils.pluginProp(fieldName));
+  }
+
+  @Then("Select File plugin property: override field data type with value: {string}")
+  public void selectFilePluginPropertyOverrideFieldDataTypeWithValue(String dataType) {
+    FileActions.selectOverrideFieldDatatype(PluginPropertyUtils.pluginProp(dataType));
+  }
+
+  @Then("Verify file plugin in-line error message for incorrect pathField value: {string}")
+  public void verifyFilePluginInLineErrorMessageForIncorrectPathField(String pathField) {
+    CdfPluginPropertiesActions.verifyPluginPropertyInlineErrorMessage
+      ("pathField",
+       PluginPropertyUtils.errorProp(E2ETestConstants.ERROR_MSG_FILE_INVALID_OUTPUTFIELD)
+         .replace("PATH_FIELD", PluginPropertyUtils.pluginProp(pathField)));
+    CdfPluginPropertiesActions.verifyPluginPropertyInlineErrorMessageColor("pathField");
+  }
+
+}
